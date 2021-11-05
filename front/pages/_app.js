c8d80e672185fd2a7a40facde403abae0c99cfe1
blefony(Trevor_Fair) <blefony.nazkorn@gmail.com>
2021-11-05 11:05:53 +0200

    update front

--- front/pages/_app.js
+++ front/pages/_app.js
@@ -1,7 +1,7 @@
 import '../styles/globals.css'
 
-function MyApp({ Component, pageProps }) {
+function WebFront({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
 
-export default MyApp
+export default WebFront
